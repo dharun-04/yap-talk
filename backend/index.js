59afe3f0325847feae34588066edb8955fd0006d
dharun-04 <dharun.b2021ecea@sece.ac.in>
2024-11-20 11:27:00 +0530

    Backend Setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 const app= express();
 dotenv.config();
 const Port=process.env.PORT || 4000;
@@ -9,4 +10,8 @@ app.get('/',(req,res)=>{
   res.send('Hello world');
 })
 app.use("/api/auth",authRoutes);
-app.listen(Port,()=> console.log(`server running in ${Port}`));
+app.listen(Port,()=> {
+  console.log(`server running in ${Port}`);
+  connectDB();
+})
+  
